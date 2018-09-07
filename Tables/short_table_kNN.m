9da00a502463097a1ff9f3231d7a1efef1ac6a3a
Mink <37825527+AhnMinkyung@users.noreply.github.com>
2018-09-07 15:26:28 +0900

    Add files via upload

--- Tables/short_table_kNN.m
+++ Tables/short_table_kNN.m
@@ -1,6 +1,6 @@
 
 %엑셀파일의 데이터 불러오기
-data=importdata('Cal6classSurfaceElectrodeData.xlsx')
+data=importdata('Cal6classShortElectrodeData.xlsx')
 
 tissue=cell(900,1)
 for m=1:150
@@ -23,13 +23,14 @@ for m=751:900
 end
 
 mag_1=data([1:900],1)
-mag_3=data([1:900],3)
 mag_6=data([1:900],6)
 mag_8=data([1:900],8)
-mag_9=data([1:900],9)
+mag_10=data([1:900],10)
 pha_1=data([1:900],11)
 pha_2=data([1:900],12)
-pha_4=data([1:900],14)
+pha_3=data([1:900],13)
+pha_6=data([1:900],16)
+pha_7=data([1:900],17)
 pha_8=data([1:900],18)
-pha_9=data([1:900],19)
-data_surface_table=table(mag_1,mag_3,mag_6,mag_8,mag_9,pha_1,pha_2,pha_4,pha_8,pha_9,tissue)
+data_short_table=table(mag_1,mag_6,mag_8,mag_10,...
+    pha_1,pha_2,pha_3,pha_6,pha_7,pha_8,tissue)
